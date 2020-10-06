01a2520ce29e451529882eda0a9d91d051b1b348
Stefan Pfahler <stefanpfahler@evosoft.com>
2020-10-06 16:00:06 +0200

    feat: enable opening tds from local storage

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import WoTLibrary from "./WoTLibrary";
 
 class App extends React.Component {
     WOT = new WoTLibrary();
+    adapterBase = "http://localhost:8080"
 
     constructor(props) {
         super(props)
-        this.state = {td: ''}
-    }
+        this.state = { offlineTD: '', exposedTD: '' }
 
+        this.onChange = this.onChange.bind(this);
+        this.lookUpExposedTD = this.lookUpExposedTD.bind(this);
+    }
 
     render() {
         return (
@@ -21,18 +24,79 @@ class App extends React.Component {
                 <main>
                     <div className="tdURL">
                         <input type="url" name="url" id="url"
-                               placeholder="https://example.com"
-                               pattern="https://.*" size="50"
-                               onKeyPress={(event) => this.runScript(event)}
-                               required/>
+                            placeholder="https://example.com"
+                            pattern="https://.*" size="50"
+                            onKeyPress={(event) => this.runScript(event)}
+                            required
+                        />
                     </div>
-                    {this.state.td && <TDViewer td={this.state.td} wot={this.WOT} /> }
+                    <div style={{ height: "10px" }}></div>
+                    <input type="file" name="file_input" id="file_input" onChange={this.onChange}></input>
+                    <div style={{ height: "10px" }}></div>
+                    {this.state.exposedTD && <TDViewer td={this.state.exposedTD} wot={this.WOT} />}
                 </main>
                 <footer>
                     <p>A tool for viewing and editing your great ThingDescription</p>
                 </footer>
-            </div>
+            </div >
         );
+    }
+
+    onChange(e) {
+        let app = this;
+
+        var reader = new FileReader();
+        reader.readAsText(new Blob([e.target.files[0]], {
+            "type": "application/json"
+        }));
+
+        reader.onload = function (event) {
+            try {
+                let td = JSON.parse(event.target.result);
+                if (app.validateTD(td)) {
+                    app.setState({ offlineTD: td })
+
+                    app.lookUpExposedTD();
+                    // app.WOT.consumeTD(td);
+                }
+            } catch (e) {
+                alert('Sorry we were not able to load the TD. Seems like it was no valid JSON object.');
+                console.log(e);
+            }
+        }
+    }
+
+    lookUpExposedTD() {
+        let app = this;
+        let url = app.adapterBase.concat("/.things");
+
+        fetch(url).then(async (json) => {
+            let dotThings = await json.json();
+            let wasFound = false;
+
+            for (let i = 0; i < dotThings.links.length; i++) {
+                const e = dotThings.links[i];
+                if (e.title !== app.state.offlineTD.title) {
+                    continue;
+                }
+
+                wasFound = true;
+                let tdURL = new URL(url.toString());
+                tdURL.href = tdURL.href.concat(e.href.replace("/.things", ""));
+
+                await app.loadTDFromURL(tdURL);
+                break;
+            }
+
+            if (!wasFound) {
+                app.setState({ exposedTD: "" });
+                app.WOT.td = "";
+
+                alert(`An exposed TD with title '${app.state.offlineTD.title}' was not found under '${url}'`);
+            }
+        }, (res) => {
+            console.log(res);
+        });
     }
 
     runScript(e) {
@@ -64,10 +128,10 @@ class App extends React.Component {
         try {
             let res = await fetch(url);
             let td = await res.json()
-            if(this.validateTD(td)) {
-            this.setState({td})
-            this.WOT.consumeTD(td);
-            }else {
+            if (this.validateTD(td)) {
+                this.setState({ exposedTD: td })
+                this.WOT.consumeTD(td);
+            } else {
                 //TODO: check if it is a list
                 //Else: Show error.
             }
